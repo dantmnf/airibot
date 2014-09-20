f893c973c86e0b9ec3f177cc10c2f94eb54d0494
dantmnf <dantmnf2@gmail.com>
2014-09-20 18:23:53 +0800

    update escaper.rb

--- escaper.rb
+++ escaper.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
 
-MEMLIMIT = 48*1024*1024
+MEMLIMIT = 96*1024*1024
 Process.setrlimit(:RSS, MEMLIMIT, MEMLIMIT) #64M <Linux 2.4.30
 Process.setrlimit(:AS, MEMLIMIT, MEMLIMIT)
 #Process.setrlimit(:MEMLOCK, MEMLIMIT, MEMLIMIT)
@@ -13,7 +13,7 @@ if Process.euid == 0
 end
 
 [:fork,:exec,:load,:require,:require_relative,:spawn,:syscall,:system,:`,:sleep,:open].each {|symb| Kernel.send(:remove_method, symb)}
-[:Process,:Thread].each {|symb| Object.send(:remove_const, symb)}
+[:Process,:Thread,:File,:FileTest].each {|symb| Object.send(:remove_const, symb)}
 #FIXME: other dangerous methods
 begin
   
