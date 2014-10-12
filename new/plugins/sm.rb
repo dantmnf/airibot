6c5219c41691b7140c7c2217d1ad0c8fcb1a47f4
dantmnf <dantmnf2@gmail.com>
2014-10-12 13:27:57 +0800

    sm plugin: URL encode query string

--- new/plugins/sm.rb
+++ new/plugins/sm.rb
@@ -11,7 +11,7 @@ class SM
   match /sm (.+)/
   
   def get_sm_message(query)
-    response = open('http://xiaofengrobot.sinaapp.com/web.php?callback=jQuery191041205509454157474_1376842442554&para=%s&_=1376842442555' % query).read
+    response = open('http://xiaofengrobot.sinaapp.com/web.php?callback=jQuery191041205509454157474_1376842442554&para=%s&_=1376842442555' % CGI::escape(query)).read
     text = JSON.parse('[' + response[5..-2] + ']').first
     text.gsub! /<br ?(\/)? ?>/, ' '
     return text
