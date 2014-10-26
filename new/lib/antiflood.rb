a3eaa2234f38638f91c78cb63594fb393d5abf4b
dantmnf <dantmnf2@gmail.com>
2014-10-26 13:03:20 +0800

    antiflooding

---
+++ new/lib/antiflood.rb
@@ -0,0 +1,78 @@
+require 'bundler/setup'
+require 'cinch'
+
+class AntiFlood
+  LOG_SIZE = 8
+  ANTIFLOOD_BURST_DURATION = 30
+  BAN_TIME = 60
+  def initialize
+    @data = {}
+    @banlist = {}
+  end
+
+  def log(msg)
+    target = msg.target.name
+    nick = 'nick=' + msg.user.nick
+    user = 'user=' + msg.user.user
+    @data[target] = {} if @data[target].nil?
+    @data[target][nick] = [] if @data[target][nick].nil?
+    @data[target][user] = [] if @data[target][user].nil?
+
+    @data[target][user].push Time.now
+    @data[target][nick].push Time.now
+
+    @data[target][nick].shift while @data[target][nick].length > LOG_SIZE
+    @data[target][user].shift while @data[target][user].length > LOG_SIZE
+  end
+
+  def check(msg)
+    return false if banned? msg
+    target = msg.target.name
+    nick = 'nick=' + msg.user.nick
+    user = 'user=' + msg.user.user
+    
+    nicklog = @data[target][nick]
+    userlog = @data[target][user]
+
+    result = false
+    result = ((nicklog.length >= LOG_SIZE) && (nicklog.last - nicklog.first < ANTIFLOOD_BURST_DURATION))
+    result ||= ((userlog.length >= LOG_SIZE) && (userlog.last - userlog.first < ANTIFLOOD_BURST_DURATION))
+
+    return result
+  end
+
+  def ban(msg, prompt=true)
+    target = msg.target.name
+    nick = 'nick=' + msg.user.nick
+    user = 'user=' + msg.user.user
+    
+    @banlist[target] = {} if @banlist[target].nil?
+    @banlist[target][nick] = Time.now + BAN_TIME
+    @banlist[target][user] = Time.now + BAN_TIME
+
+    m.reply('Pia!<(=ｏ ‵-′)ノ☆', true) if prompt
+    
+  end
+
+  def banned?(msg)
+    target = msg.target.name
+    nick = 'nick=' + msg.user.nick
+    user = 'user=' + msg.user.user
+    
+    result = (@banlist[target][nick].is_a? Time && @banlist[target][nick] > Time.now)
+    result ||= (@banlist[target][user].is_a? Time && @banlist[target][user] > Time.now)
+    
+    return result
+  end
+  
+  def log_check_and_ban(msg, prompt=true)
+    return false if banned? msg
+    log msg
+    checkresult = check msg
+    ban(msg, prompt) if checkresult == true
+    return checkresult
+  end
+end
+
+
+
